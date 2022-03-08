7c28beb0001a85234166cc2746b304513facd199
Luiz Claro <lfclaro@gmail.com>
2022-03-08 04:58:09 -0500

    adding files

---
+++ LuizClaro_N01459385_A1/Models/ScaleFactor.swift
@@ -0,0 +1,13 @@
+//
+//  ScaleFactor.swift
+//  LuizClaro_N01459385_A1
+//
+//  Created by Luiz Fernando Reis on 2022-03-07.
+//
+
+import SwiftUI
+
+struct ScaleFactor {
+    var w = UIScreen.main.bounds.size.width
+    var h = UIScreen.main.bounds.size.height
+}
